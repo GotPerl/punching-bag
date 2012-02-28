19642409834bf76c2e1998284305d13a85dff04a
Jordan Conley <jordanconley1@gmail.com>
2012-02-27 21:05:42 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PunchingBag::Application.config.secret_token = '3d9eb053270d35dab9ca1c2dc0caeaee6f2a09ee99e7a5f6c69f62c0194ed9b18fb8e30bdc008b29bf971ad1be79e692c5c4db03ddb85735959dd8d735460042'
